c9dc23a4163b6ddd0a69b5596b9f53974feb3895
valekpot92 <148702643+valekpot92@users.noreply.github.com>
2023-11-15 23:59:32 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+
+namespace biblioteka
+{
+
+    class program
+    {
+
+        static void Main(string[] args)
+        {
+           
+             people klient= new people();
+            klient.name = "Petr";
+            klient.supername = "Ivanov";
+            klient.id = 1;  
+            Console.WriteLine("Имя: " + klient.name);
+            Console.WriteLine("Фамилия: "+ klient.supername);
+            Console.WriteLine("ID: "+ klient.id );
+        book booking = new book();
+            booking.author_1 = "Пушкин";
+            booking.id = 236;
+            booking.dolg = 3;
+            Console.WriteLine("Вы взяли автора:" +booking.author_1);
+            Console.WriteLine("ID: " + booking.id);
+            Console.WriteLine("Ваша задолжность составляет:" + booking.dolg + "книги");
+
+        }
+        
+        
+    }
+
+}
---
+++ ConsoleApp1/book.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace biblioteka
+{
+    class people
+    {
+        public string name;
+        public string supername;
+        public int id;
+
+
+    }
+    class book : people {
+       public string author_1;
+       public int dolg;
+
+    
+    }
+
+  
+    
+
+
+}
